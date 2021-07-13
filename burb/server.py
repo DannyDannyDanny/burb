aae18e6503709074bf0f35dcfc455e1f668da620
DannyDannyDanny <dtho>
2021-07-13 18:25:33 +0200

    :snake: write current and parent dir contents + absolute to files (log)

--- burb/server.py
+++ burb/server.py
@@ -1,1 +1,25 @@
-print('hello world')
+from pathlib import Path
+
+# list dirs in current folder and write to file (files_in_here.txt)
+mypath = '.'
+mydirs = "\n".join([str(p) for p in Path(mypath).iterdir()])
+myfilename = 'files_in_here.txt'
+myfile = Path(myfilename)
+mytext = f'testing dir: {mypath}' + mydirs
+myfile.write_text(mytext)
+
+# list dirs in parent folder and write to file (files_in_parent.txt)
+mypath = '..'
+mydirs = "\n".join([str(p) for p in Path(mypath).iterdir()])
+myfilename = 'files_in_parent.txt'
+myfile = Path(myfilename)
+mytext = f'testing dir: {mypath}' + mydirs
+myfile.write_text(mytext)
+
+# write absolute file path to file (absolute.txt)
+mypath = '..'
+mydirs = "\n".join([str(p) for p in Path(mypath).iterdir()])
+myfilename = 'absolute.txt'
+myfile = Path(myfilename)
+mytext = str(myfile.absolute())
+myfile.write_text(mytext)
